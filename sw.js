24cfe9209540e90107749575d8867d71f7d4925b
takuZZZ <u973271e@ecs.osaka-u.ac.jp>
2025-02-14 22:20:27 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'fortune-app-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+// インストール時にキャッシュする
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+// フェッチ時にキャッシュから応答する
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
